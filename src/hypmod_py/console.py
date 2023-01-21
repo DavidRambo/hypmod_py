bc37522d1cf7fb3091a1f52fd3855ebf0d454071
David Rambo <davrambo@gmail.com>
2023-01-21 10:10:53 -0800

    Retrieves random page from Wikipedia.

--- src/hypmod_py/console.py
+++ src/hypmod_py/console.py
@@ -1,11 +1,25 @@
 # src/hypmod_py/console.py
+import textwrap  # stdlib module for wrapping console text
+
 import click
+import requests
 
 from . import __version__
+
+# REST API of Wikipedia's /page/random/sammary
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
     """The hypermodern Python project."""
-    click.echo("Hello, world.")
+    with requests.get(API_URL) as response:
+        response.raise_for_status()  # check HTTP status code
+        data = response.json()  # retrieved data in json format
+
+        title = data["title"]
+        extract = data["extract"]
+
+        click.secho(title, fg="green")
+        click.echo(textwrap.fill(extract))
