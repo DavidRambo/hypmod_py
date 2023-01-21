fc9f338babdf9f5e7ec1ac57a4a39e4e1b1aff0f
David Rambo <davrambo@gmail.com>
2023-01-21 10:34:34 -0800

    Setup basic test

---
+++ tests/test_console.py
@@ -0,0 +1,15 @@
+# tests/test_console.py
+import click.testing
+import pytest
+
+from hypmod_py import console
+
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+def test_main_succeeds():
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
