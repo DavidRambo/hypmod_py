3d03122e8d0192ecac09c80b973dcfabfb1736a6
David Rambo <davrambo@gmail.com>
2023-01-23 11:31:36 -0800

    Set Nox to use posargs

--- noxfile.py
+++ noxfile.py
@@ -5,5 +5,6 @@ import nox
 @nox.session(python=["3.10", "3.11"])
 def tests(session):
     """Basic nox test."""
+    args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
-    session.run("poetry", "run", "pytest", "--cov")
+    session.run("poetry", "run", "pytest", *args, external=True)
