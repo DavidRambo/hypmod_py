49e8af5b5358bba80db9077fec3a37fc6cd11db1
David Rambo <davrambo@gmail.com>
2023-02-02 20:25:01 -0800

    Add validation error test

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,5 +1,7 @@
 # tests/test_wikipedia.py
 from hypmod_py import wikipedia
+import pytest
+import click
 
 
 def test_random_page_returns_page(mock_requests_get):
@@ -12,3 +14,9 @@ def test_random_page_uses_given_language(mock_requests_get):
     wikipedia.random_page(language="de")
     args, _ = mock_requests_get.call_args
     assert "de.wikipedia.org" in args[0]
+
+
+def test_random_page_handles_validation_errors(mock_requests_get) -> None:
+    mock_requests_get.return_value.__enter__.return_value.json.return_value = None
+    with pytest.raises(click.ClickException):
+        wikipedia.random_page()
